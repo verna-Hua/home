215cb611271a1a1164060c6090fec8984e696ca3
Verna <a0903580091@gmail.com>
2025-01-14 15:04:19 +0800

    更新了網站內容

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-if (targetElement) {
-    targetElement.scrollIntoView({
-        behavior: 'smooth'
-    });
-} else {
-    console.error('目標元素未找到！');
+function scrollToTarget() {
+    const targetElement = document.querySelector('#my-target');
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    } else {
+        console.error('目標元素未找到！');
+    }
 }
